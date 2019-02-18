380f51fb05cdaa833005999f8579a6ce8a4e9f3c
Paweł Iwan <piwan@akamai.com>
2019-02-18 22:10:54 +0100

    Fix improper account balance default value in constructor

--- account.py
+++ account.py
@@ -3,12 +3,12 @@ from money import Monetary
 
 class Account:
 
-    def __init__(self, owner_first_name, owner_last_name, owner_address, ban, balance=Monetary(0, "PLN")):
+    def __init__(self, owner_first_name, owner_last_name, owner_address, ban, balance=None):
         self.owner_first_name = owner_first_name
         self.owner_last_name = owner_last_name
         self.owner_address = owner_address
         self.ban = ban
-        self.balance = balance
+        self.balance = balance if balance is not None else Monetary(0, "PLN")
 
     def __repr__(self):
         return f"Account {self.ban}, balance {self.balance}"
